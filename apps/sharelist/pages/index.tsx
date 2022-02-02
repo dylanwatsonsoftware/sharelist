410e26aeb5b8960b9d0cffeaaddf44d0b1df6769
Dylan Watson <lotsabackscatter@gmail.com>
2022-02-03 00:03:18 +0800

    Add repo link

--- apps/sharelist/pages/index.tsx
+++ apps/sharelist/pages/index.tsx
@@ -57,6 +57,10 @@ export function Index() {
                 d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
               />
             </svg>
+            by{' '}
+            <a href="https://github.com/dylanwatsonsoftware/sharelist">
+              @dylanwatsonsoftware
+            </a>
           </p>
         </div>
       </div>
