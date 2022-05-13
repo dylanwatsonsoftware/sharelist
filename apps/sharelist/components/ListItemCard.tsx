327e3e4f0de04e8343fd95dcf426b6b5178fb215
Dylan Watson <lotsabackscatter@gmail.com>
2022-05-13 10:30:22 +0800

    chore: Extract hook

--- apps/sharelist/components/ListItemCard.tsx
+++ apps/sharelist/components/ListItemCard.tsx
@@ -15,7 +15,7 @@ const ImageHolder = styled.div`
   margin-right: 10px;
 `;
 
-const ListItemCard = ({ item }: { item: ListItem }) => {
+const useImage = (item: ListItem) => {
   const { data, error } = useSWR<MovieDBResult>(
     'https://api.themoviedb.org/3/search/multi?api_key=fff3eb2aeadd24e26460b0f96ea7b056&language=en-US&query=' +
       encodeURIComponent(item.name) +
@@ -24,20 +24,35 @@ const ListItemCard = ({ item }: { item: ListItem }) => {
   );
 
   const gameResult = useSWR<any>(
-    'https://api.boardgameatlas.com/api/search?order_by=rank&ascending=false&limit=1&pretty=true&client_id=' + process.env.NEXT_PUBLIC_BOARDGAME_CLIENT_ID + '&name=' +
+    'https://api.boardgameatlas.com/api/search?order_by=rank&ascending=false&limit=1&pretty=true&client_id=' +
+      process.env.NEXT_PUBLIC_BOARDGAME_CLIENT_ID +
+      '&name=' +
       encodeURIComponent(item.name),
     fetcher
   );
-
-  if (error) return <div>failed to load {error}</div>;
-  if (!data) return <div>loading...</div>;
 
   const firstResult = !error && data?.results?.[0];
   const firstGame = !gameResult.error && gameResult.data?.games?.[0];
   const name = firstResult?.name || firstResult?.title;
 
-  const image = firstResult && name?.toLowerCase().includes(item.name.toLowerCase()) && firstResult?.vote_count > 400 ? 'https://image.tmdb.org/t/p/w92/' + firstResult?.poster_path : firstGame?.name?.toLowerCase().includes(item.name.toLowerCase()) ? firstGame?.images?.small : undefined;
-  
+  const image =
+    firstResult &&
+    name?.toLowerCase().includes(item.name.toLowerCase()) &&
+    firstResult?.vote_count > 400
+      ? 'https://image.tmdb.org/t/p/w92/' + firstResult?.poster_path
+      : firstGame?.name?.toLowerCase().includes(item.name.toLowerCase())
+      ? firstGame?.images?.small
+      : undefined;
+
+  return { image, error, data };
+};
+
+const ListItemCard = ({ item }: { item: ListItem }) => {
+  const { error, data, image } = useImage(item);
+
+  if (error) return <div>failed to load {error}</div>;
+  if (!data) return <div>loading...</div>;
+
   return (
     <a
       href={`https://www.google.com/search?q=${item.name}`}
@@ -50,11 +65,7 @@ const ListItemCard = ({ item }: { item: ListItem }) => {
         <GoPrimitiveDot />
       ) : (
         <ImageHolder>
-          <Image
-            src={image}
-            alt={item.name}
-            layout="fill"
-          />
+          <Image src={image} alt={item.name} layout="fill" />
         </ImageHolder>
       )}
       <span>
